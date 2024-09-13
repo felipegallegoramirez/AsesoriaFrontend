c1c9d1423a9e5fe190ed9a4208ae8b37a66c1b61
Felipe Gallego <felipegallego600@gmail.com>
2024-09-12 22:42:58 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
