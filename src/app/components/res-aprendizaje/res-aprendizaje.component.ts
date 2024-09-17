ebcb481f4c2de7b91a35809e1f719b56638d293c
Felipe Gallego <felipegallego600@gmail.com>
2024-09-16 20:32:43 -0500

    res-aprendizaje

--- src/app/components/res-aprendizaje/res-aprendizaje.component.ts
+++ src/app/components/res-aprendizaje/res-aprendizaje.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-res-aprendizaje',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './res-aprendizaje.component.css'
 })
 export class ResAprendizajeComponent {
+  form: FormGroup;
+  programas: any[] = [];
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      id_restultado: ['', [Validators.required, Validators.maxLength(50)]],
+      competencia: ['', [Validators.required, Validators.maxLength(600)]],
+      tipo_saber: ['', [Validators.required, Validators.maxLength(10)]],
+      saber_asociado: ['', [Validators.required, Validators.maxLength(255)]],
+      taxonomia: ['', [Validators.required, Validators.maxLength(10)]],
+      dominio_tratar: ['', [Validators.required, Validators.maxLength(20)]],
+      nivel_dominio: ['', [Validators.required, Validators.maxLength(20)]],
+      verbo: ['', [Validators.required, Validators.maxLength(20)]],
+      resultado_ap: ['', [Validators.required, Validators.maxLength(600)]],
+      id_programa: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
