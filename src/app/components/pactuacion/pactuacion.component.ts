e336ac6d66c40ddcc5177d794c80bd4ca0c69af3
Felipe Gallego <felipegallego600@gmail.com>
2024-09-16 20:18:53 -0500

    PActuacion

--- src/app/components/pactuacion/pactuacion.component.ts
+++ src/app/components/pactuacion/pactuacion.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pactuacion',
   templateUrl: './pactuacion.component.html',
   styleUrl: './pactuacion.component.css'
 })
-export class PActuacionComponent {
+export class PActuacionComponent implements OnInit {
+  form: FormGroup;
+  programas: any[] = [];
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      id_programa: ['', Validators.required],
+      id: ['', [Validators.required, Validators.maxLength(50)]],
+      p_actuacion: ['', [Validators.required, Validators.maxLength(600)]]
+    });
+  }
+
+  ngOnInit() {
+    // Aquí deberías cargar los programas desde tu servicio
+    this.programas = [
+      { id: '1', nombre: 'Programa 1' },
+      { id: '2', nombre: 'Programa 2' },
+      // ... más programas
+    ];
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      // Aquí deberías enviar los datos a tu servicio
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
