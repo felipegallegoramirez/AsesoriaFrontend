40864176a83fc8de422f2df02fd1b06f5205d45b
Felipe Gallego <felipegallego600@gmail.com>
2024-09-16 20:36:11 -0500

    est-mesocurricular

--- src/app/components/est-mesocurricular/est-mesocurricular.component.ts
+++ src/app/components/est-mesocurricular/est-mesocurricular.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-est-mesocurricular',
   templateUrl: './est-mesocurricular.component.html',
   styleUrl: './est-mesocurricular.component.css'
 })
-export class EstMesocurricularComponent {
+export class EstMesocurricularComponent implements OnInit {
+  form: FormGroup;
+  estrategias: string[] = ['Estrategia 1', 'Estrategia 2', 'Estrategia 3'];
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      resultado_ap: ['', [Validators.required, Validators.maxLength(600)]],
+      estrategia_programa: ['', [Validators.required]],
+      descripcion: ['', [Validators.required, Validators.maxLength(600)]]
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
