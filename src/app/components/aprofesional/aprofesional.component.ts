dec129ccee5a9b2c9631d7572113788ee738b5c9
Felipe Gallego <felipegallego600@gmail.com>
2024-09-16 20:14:08 -0500

    aprofesional

--- src/app/components/aprofesional/aprofesional.component.ts
+++ src/app/components/aprofesional/aprofesional.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-aprofesional',
   templateUrl: './aprofesional.component.html',
   styleUrl: './aprofesional.component.css'
 })
-export class AProfesionalComponent {
+export class AProfesionalComponent implements OnInit{
+  form: FormGroup;
+  programas: any[] = [];
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      id_programa: ['', Validators.required],
+      id: ['', [Validators.required, Validators.maxLength(50)]],
+      a_profesional: ['', [Validators.required, Validators.maxLength(300)]]
+    });
+  }
+
+  ngOnInit() {
+    // Aquí deberías cargar los programas desde tu servicio
+    this.programas = [
+      { id: '1', nombre: 'Programa 1' },
+      { id: '2', nombre: 'Programa 2' },
+      // ... más programas
+    ];
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      // Aquí deberías enviar los datos a tu servicio
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
